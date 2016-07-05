fbc5cdd346bc0411993cde1b88e7f929cf8f1089
Ryan Lim <rlim1812@gmail.com>
2016-07-05 12:24:58 -0700

    This is a fun app that I made that converts a cat's age from human years to cat years

--- Cat Years/ViewController.swift
+++ Cat Years/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var catAgeTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func FindAge(sender: AnyObject) {
+        var catAge = Int(catAgeTextField.text!)!
+        catAge = catAge * 7
+        resultLabel.text = "Your cat is \(catAge) in cat years"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
